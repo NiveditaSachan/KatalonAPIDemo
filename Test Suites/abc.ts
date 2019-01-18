8af3889d4d0ef310dbc455c97aff8ccd5c4513c3
nivedita <nivedita@192.168.0.103>
2019-01-18 10:17:19 +0530

    addinng abc

---
+++ Test Suites/abc.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>abc</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9a32ceed-9294-4098-a440-bf76439ae1c9</testSuiteGuid>
+</TestSuiteEntity>
