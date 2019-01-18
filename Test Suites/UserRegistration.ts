9043a54f0a7f3c952d024656846bc8c0cfb4435a
nivedita <nivedita@192.168.0.103>
2019-01-18 10:20:49 +0530

    Adding UserRegistration object rep

---
+++ Test Suites/UserRegistration.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserRegistration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>db62a84f-2ab2-4df6-b6b3-4685dbe0d6c9</testSuiteGuid>
+</TestSuiteEntity>
