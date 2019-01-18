ef5911f497d8ded1b239bb585e51a41bb5b58a8b
nivedita <nivedita@192.168.0.103>
2019-01-18 10:28:17 +0530

    First test case

--- Test Suites/UserRegistration.ts
+++ Test Suites/UserRegistration.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>db62a84f-2ab2-4df6-b6b3-4685dbe0d6c9</testSuiteGuid>
+   <testCaseLink>
+      <guid>cc182a5b-114d-44c7-9b4d-c5e1078754e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserRegistration/RegistrationFailure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b66d0535-fd29-48a8-87d9-d66a44e4bb66</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserRegistration/registrationSuccess</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
